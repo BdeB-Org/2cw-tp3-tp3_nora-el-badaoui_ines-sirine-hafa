0d6586d142a49a1a4f11130025b04111bcb28e0a
InesSirine <2372947@bdeb.qc.ca>
2024-05-31 22:59:00 -0400

    ajustement api

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,31 +4,10 @@ function afficherMerci(event)
     document.getElementById('contenu').innerHTML = '<h2> Merci pour votre commentaire ! </h2>';
 }
 
-fetch('http://localhost:8080/books')
-.then(response => response.json())
-.then(data => {
-const books = data.books;
-const booksContainer = document.getElementById('books-container');
-books.forEach(book =>
-    {
-        const bookElement = document.createElement('div');
-bookElement.innerHTML = `<h3>${book.title}</h3>
-<p> Id auteur: ${book.author_id} </p>
-<p> Date de publication: ${book.publication_date}</p>
-<p> Description: ${book.description}</p>
-<img src="${book.cover_image_url}" alt="${book.title}" style="width:100px;height:150px;">`
-
-booksContainer.appendChild(book)
-})
-
-
-
-})
-
 
 const formChercher = document.getElementById('formChercher');
 
-// Écouteur d'événement pour le formulaire de recherche
+
 formChercher.addEventListener('submit', (e) => {
   e.preventDefault();
   const titre = document.getElementsByName('titre')[0].value;
@@ -39,7 +18,7 @@ formChercher.addEventListener('submit', (e) => {
       const books = data.books;
 
 
-      // Affichage des livres trouvés
+
       if (books.length > 0) {
         books.forEach((book) => {
           const bookElement = document.createElement('div');
@@ -53,7 +32,7 @@ formChercher.addEventListener('submit', (e) => {
           booksContainer.appendChild(bookElement);
         });
       } else {
-        // Affichage d'un message si aucun livre n'a été trouvé
+
         const message = document.createElement('p');
         message.textContent = 'Aucun livre trouvé avec ce titre.';
         booksContainer.appendChild(message);
